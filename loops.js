ff5e6ac085fc0f2a118d74e28df7ad72fbd9a69d
Roxanne Baldwin <rox.m.baldwin@gmail.com>
2017-11-22 13:29:08 -0700

    🐛  refactored fn acronym using reduce

--- loops.js
+++ loops.js
@@ -3,16 +3,16 @@
 // If you pass ["Sue", "Will"] it should return "Sue and Will"
 // If you pass ["Sue", "Will", "Rachel"] it should return "Sue, Will and Rachel"
 
-function Sentence(arr) {
-  var result = "";
-  if (arr.length === 0) {
-      return result;
-  }
-  for (var i = 0; i < arr.length - 2; i++) {
-    result += arr[i] + ", ";
-  }
-  return result + arr[arr.length - 2] + " and " + arr[arr.length - 1];
-}
+// function toSentence(arr) {
+//   var result = "";
+//   if (arr.length === 0) {
+//       return result;
+//   }
+//   for (var i = 0; i < arr.length - 2; i++) {
+//     result += arr[i] + ", ";
+//   }
+//   return result + arr[arr.length - 2] + " and " + arr[arr.length - 1];
+// }
 
 const toSentence = function(arr){
   let joinNames = arr.reduce((sentance, name, currentIndex, names) => {
@@ -25,16 +25,16 @@ const toSentence = function(arr){
     }
     return sentance
   }, '')
-  console.log(joinNames)
+  return joinNames
 }
 
-let nameArr = ["Sue", "Will", "Rachel", "Roxx"]
-
-toSentence(nameArr)
+// let nameArr = ["Sue", "Will", "Rachel", "Roxx"]
+//
+// toSentence(nameArr)
 
 // If you pass 1 it should return [1]
 // If you pass 3 it should return [1,2,3]
-function arrayOfNumbers(num) {
+function arrayONumbers(num) {
     var result = [];
     for (var i = 1; i <= num; i++) {
         result.push(i);
@@ -43,14 +43,22 @@ function arrayOfNumbers(num) {
 }
 
 // If you pass ["Sue", "Will"] it should return "SW"
-// If you pass ["Java", Script", "Object", "Notation"] it should return "JSON"
+// If you pass ["Java", "Script", "Object", "Notation"] it should return "JSON"
 
-function acronym(arr) {
-    var result = "";
-    for (var i = 0; i < arr.length; i++) {
-        result += arr[i].charAt();
-    }
-    return result;
+// function toAcronym(arr) {
+//     var result = "";
+//     for (var i = 0; i < arr.length; i++) {
+//         result += arr[i].charAt();
+//     }
+//     return result;
+// }
+
+const acronym = function(arr){
+  let abbreviated = arr.reduce((label, word) => {
+    label += word[0]
+    return label
+  }, '')
+  return abbreviated
 }
 
 // Given [1, 3, 2, 1, 3], 1, and 4, then return [4, 3, 2, 4, 3]
